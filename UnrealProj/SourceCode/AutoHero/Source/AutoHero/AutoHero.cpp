8526880437bd5fbe1d08d7bb3d1afd4b7b41b31f
dannylam2502 <dannylam2502@gmail.com>
2023-02-14 20:31:17 -0800

    Delete

--- UnrealProj/SourceCode/AutoHero/Source/AutoHero/AutoHero.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AutoHero.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AutoHero, "AutoHero" );
-
-DEFINE_LOG_CATEGORY(LogAutoHero)
- 
