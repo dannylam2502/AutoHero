11e7976b6a305548e1458558ff988dcf40e12083
dannylam2502 <dannylam2502@gmail.com>
2024-01-11 00:28:38 -0800

    Homing Arrow Set Up

--- AutoHero/Source/AutoHero/Private/Core/Actors/BaseProjectile.cpp
+++ AutoHero/Source/AutoHero/Private/Core/Actors/BaseProjectile.cpp
@@ -22,45 +22,13 @@ ABaseProjectile::ABaseProjectile()
 
     //Definition for the SphereComponent that will serve as the Root component for the projectile and its collision.
     SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
-    SphereComponent->InitSphereRadius(37.5f);
-    SphereComponent->SetCollisionProfileName(TEXT("BlockAllDynamic"));
     RootComponent = SphereComponent;
-
-    //Registering the Projectile Impact function on a Hit event.
-    if (GetLocalRole() == ROLE_Authority)
-    {
-        SphereComponent->OnComponentHit.AddDynamic(this, &ABaseProjectile::OnProjectileImpact);
-    }
 
     //Definition for the Mesh that will serve as your visual representation.
     StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
     StaticMesh->SetupAttachment(SphereComponent);
-	StaticMesh->SetCollisionProfileName(EName::None);
 	
     //Definition for the Projectile Movement Component.
     ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovement"));
     ProjectileMovementComponent->SetUpdatedComponent(SphereComponent);
-}
-
-// Called when the game starts or when spawned
-void ABaseProjectile::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ABaseProjectile::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-void ABaseProjectile::OnProjectileImpact(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-    UE_LOG(LogTemp, Warning, TEXT("Hit Other Actor"))
-    if (OtherActor)
-    {
-    }
-	//Destroy();
 }
--- AutoHero/Source/AutoHero/Public/Core/Actors/BaseProjectile.h
+++ AutoHero/Source/AutoHero/Public/Core/Actors/BaseProjectile.h
@@ -15,15 +15,6 @@ public:
 	// Sets default values for this actor's properties
 	ABaseProjectile();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-public:
     // Sphere component used to test collision.
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     class USphereComponent* SphereComponent;
@@ -35,13 +26,4 @@ public:
     // Movement component for handling projectile movement.
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
     class UProjectileMovementComponent* ProjectileMovementComponent;
-
-    // Particle used when the projectile impacts against another object and explodes.
-    UPROPERTY(EditAnywhere, Category = "Effects")
-    class UParticleSystem* ExplosionEffect;
-
-protected:
-
-    UFUNCTION(Category = "Projectile")
-    void OnProjectileImpact(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 };
