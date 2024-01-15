c8642794c6e12e53804130b75c30ac9611571c5c
dannylam2502 <dannylam2502@gmail.com>
2024-01-14 23:14:35 -0800

    Fix a bug when animation complete end the ability that makes the event failed

--- AutoHero/Source/AutoHero/Private/UI/HealthBar.cpp
+++ AutoHero/Source/AutoHero/Private/UI/HealthBar.cpp
@@ -18,23 +18,27 @@ void UHealthBar::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
 	{
 		return;
 	}
-	float CurrentHealth = OwnerUnit->GetCurrentHealth();
-	float MaxHealth = OwnerUnit->GetMaxHealth();
-	if (MaxHealth > 0)
-	{
-		HealthBar->SetPercent(CurrentHealth / MaxHealth);
-		// todo temp
-		ManaBar->SetPercent(CurrentHealth / MaxHealth);
-	}
-
 	// get current attributes
 	const UUnitAttributeSet* Attributes = OwnerUnit->GetAttributes();
 	
+	const float CurrentHealth = Attributes->GetHealth();
+	const float MaxHealth = Attributes->GetMaxHealth();
+	const float CurrentMana = Attributes->GetMana();
+	const float MaxMana = Attributes->GetMaxMana();
+	if (MaxHealth > 0)
+	{
+		HealthBar->SetPercent(CurrentHealth / MaxHealth);
+	}
+	if (MaxMana > 0)
+	{
+		ManaBar->SetPercent(CurrentMana / MaxMana);
+	}
+	
 	FNumberFormattingOptions Opts;
 	Opts.SetMaximumFractionalDigits(0);
-	CurrentHealthLabel->SetText(FText::AsNumber(Attributes->GetHealth(), &Opts));
-	MaxHealthLabel->SetText(FText::AsNumber(Attributes->GetMaxHealth(), &Opts));
+	CurrentHealthLabel->SetText(FText::AsNumber(CurrentHealth, &Opts));
+	MaxHealthLabel->SetText(FText::AsNumber(MaxHealth, &Opts));
 
-	CurrentManaLabel->SetText(FText::AsNumber(Attributes->GetMana(), &Opts));
-	MaxManaLabel->SetText(FText::AsNumber(Attributes->GetMaxMana(), &Opts));
+	CurrentManaLabel->SetText(FText::AsNumber(CurrentMana, &Opts));
+	MaxManaLabel->SetText(FText::AsNumber(MaxMana, &Opts));
 }
