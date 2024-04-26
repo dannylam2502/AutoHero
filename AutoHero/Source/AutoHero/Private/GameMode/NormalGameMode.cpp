8f824a4322386353d6452debb4490b129b63ded5
dannylam2502 <dannylam2502@gmail.com>
2024-04-26 02:33:15 -0700

    Fix bugs Upgrade Project to 5.4

--- AutoHero/Source/AutoHero/Private/GameMode/NormalGameMode.cpp
+++ AutoHero/Source/AutoHero/Private/GameMode/NormalGameMode.cpp
@@ -10,20 +10,20 @@
 void ANormalGameMode::Spawn()
 {
 	return;
-	if (ActorToSpawn)
-	{
-		UWorld* world = GetWorld();
-		if (world)
-		{
-			FActorSpawnParameters spawnParams;
-			spawnParams.Owner = UGameplayStatics::GetPlayerController(world, 0);
-			spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
-			FRotator rotator;
-			FVector spawnLocation(0.0f, -270.0f, 89.0f);
-			ABaseUnit* spawnedUnit = world->SpawnActor<ABaseUnit>(ActorToSpawn, spawnLocation, rotator, spawnParams);
-			spawnedUnit->ETeam = EActorTeam::Red;
-			if (GEngine)
-				GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "ANormalGameMode::Spawn");
-		}
-	}
+	// if (ActorToSpawn)
+	// {
+	// 	UWorld* world = GetWorld();
+	// 	if (world)
+	// 	{
+	// 		FActorSpawnParameters spawnParams;
+	// 		spawnParams.Owner = UGameplayStatics::GetPlayerController(world, 0);
+	// 		spawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+	// 		FRotator rotator;
+	// 		FVector spawnLocation(0.0f, -270.0f, 89.0f);
+	// 		ABaseUnit* spawnedUnit = world->SpawnActor<ABaseUnit>(ActorToSpawn, spawnLocation, rotator, spawnParams);
+	// 		spawnedUnit->ETeam = EActorTeam::Red;
+	// 		if (GEngine)
+	// 			GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "ANormalGameMode::Spawn");
+	// 	}
+	// }
 }
